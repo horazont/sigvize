416d21f0039343711bdf8c144ceb330080b537f5
Jonas Wielicki <j.wielicki@sotecware.net>
2011-03-09 00:58:37 +0100

    * Initialized project

---
+++ modules/forms/main.pas
@@ -0,0 +1,26 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs; 
+
+type
+  TMainForm = class(TForm)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
